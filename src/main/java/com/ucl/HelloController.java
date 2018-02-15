600a8fd3d4731c0d3599ba93d683f0920b0b2902
Majid Khan <uczmmkh@ad.ucl.ac.uk>
2018-02-15 13:02:13 +0000

    As a test added a import from isdutils

--- src/main/java/com/ucl/HelloController.java
+++ src/main/java/com/ucl/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import edu.ucl.isd.postauthentication.ShibbolethPostAuthentication;
 
 @Controller
 public class HelloController {
